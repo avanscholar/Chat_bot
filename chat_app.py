902f807676f3ecf7d28ba7dbf9dcbfbcf1436e12
Avan Kumar <99405356+avanscholar@users.noreply.github.com>
2024-09-11 14:43:40 -0700

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
-from llama_index.llms.openai import OpenAI
+from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+from llama_index.llms import OpenAI
 import openai
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core.prompts.prompts import SimpleInputPrompt
