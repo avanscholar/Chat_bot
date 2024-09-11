d61b22dc53369b56f91158bd8e6c92dd3db3a91b
Avan Kumar <99405356+avanscholar@users.noreply.github.com>
2024-09-11 14:38:54 -0700

    Add files via upload

---
+++ chat_app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+from llama_index.llms.openai import OpenAI
+import openai
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core.prompts.prompts import SimpleInputPrompt
+from llama_index.core import Settings
+
+# openai.api_key = st.secrets.openai_key
+st.header("Chat with the Streamlit docs 💬 📚")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+    ]
+
+import torch
+system_prompt="You are an expert in environmental science providing accurate and detailed answers with all components details and numerical dictionary."
+
+## Default format supportable by LLama2
+query_wrapper_prompt=SimpleInputPrompt("<|USER|>{query_str}<|ASSISTANT|>")
+llm = HuggingFaceLLM(
+    context_window=4096,
+    max_new_tokens=512,
+    generate_kwargs={"temperature": 0.1, "do_sample": False},
+    system_prompt=system_prompt,
+    query_wrapper_prompt=query_wrapper_prompt,
+    tokenizer_name="avankumar/Chatbot_plastic_LLM",
+    model_name="avankumar/Chatbot_plastic_LLM",
+    device_map="auto",
+    # uncomment this if using CUDA to reduce memory usage
+    model_kwargs={"torch_dtype": torch.float16 , "load_in_8bit":False}  #Keep True if we want to store the model in 8bit
+)
+
+
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from llama_index.core import ServiceContext
+# from llama_index.embeddings import LangchainEmbedding
+from llama_index.embeddings.langchain import LangchainEmbedding
+
+embed_model=LangchainEmbedding(
+    HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2"))
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./Data_RAG", recursive=True)
+        docs = reader.load_data()
+        Settings.llm = llm
+        Settings.embed_model = embed_model
+        index = VectorStoreIndex.from_documents(docs, service_context=Settings)
+        return index
+
+index = load_data()
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
+
+
+
